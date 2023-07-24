b5543600469575e9c8731ac453c1fa8061d87114
Jordan <138933858+Roebot926@users.noreply.github.com>
2023-07-24 17:03:32 -0400

    Replit copy

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -1,9 +1,21 @@
 const Message = require('../message.js');
 const Command = require('../command.js');
 
-// NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
-//       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
-
 describe("Message class", function() {
+    
+  it("throws error if a name is NOT passed into the constructor as the first parameter", function() {
+    expect( function() { new Message();}).toThrow(new Error('Name is required.'));
+  });
 
+  it('constructor sets name', function(){
+    let message = new Message('Test message with two commands');
+    expect(message.name).toEqual('Test message with two commands');
+  })
+
+  it('contains a commands array passed into the constructor as 2nd argument', function(){
+    let commands = [new Command('MOVE', 12000), new Command('STATUS_CHECK')]
+    let commandTypes = new Message("Test message with two commands",commands);
+    expect(commandTypes.commands).toEqual(commands);
+  })
+  
 });
